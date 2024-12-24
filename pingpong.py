4a53b7b246af0b1201c23f72847df1afc1047b10
fbchausar <fbchausar@gmail.com>
2024-12-24 11:55:39 +0700

    Add files via upload
    v1.2

--- pingpong.py
+++ pingpong.py
@@ -1,5 +1,19 @@
 from pygame import*
 
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image, player_x,player_y,player_speed,wight,height):
+        super().__init__()
+        self.image = transform.scale(image.load(player.image),(wight,height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 #game scene:
 back = (200,255,255)
 win_width =600
@@ -18,3 +32,4 @@ while game:
             game =False
     display.update()
     clock.tick(FPS)
+
